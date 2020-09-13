e3087a70df6651015353725b39dfb406e8ed4fa8
Diana-nyamai <dnyamai.dn@gmail.com>
2020-09-14 00:57:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Carousel} from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel>
+    <Carousel.Item interval={1000}>
+      <img
+        className="d-block w-100"
+        src="nature1.jpg"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>Sunset</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={500}>
+      <img
+        className="d-block w-100"
+        src="nature2.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Green land</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="nature3.jpg"
+        alt="Third slide"
+      />
+      <Carousel.Caption>
+        <h3>Hills</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   );
 }
 
