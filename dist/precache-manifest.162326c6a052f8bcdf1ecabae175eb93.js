2d812107a29e49b2eba15d1f1c6acacea18d571e
Diana-nyamai <dnyamai.dn@gmail.com>
2020-10-11 12:22:46 +0300

    update

---
+++ dist/precache-manifest.162326c6a052f8bcdf1ecabae175eb93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cca4794063d757c690f5b29e5ffe39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589213f24d9b4176fcd2",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ebe7fd7c6e261c0cd9ae",
+    "url": "/static/css/main.dddba99d.chunk.css"
+  },
+  {
+    "revision": "589213f24d9b4176fcd2",
+    "url": "/static/js/2.c2a30eed.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c2a30eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe7fd7c6e261c0cd9ae",
+    "url": "/static/js/main.1dd29cb8.chunk.js"
+  },
+  {
+    "revision": "239a4a768db6f4d8ebd9",
+    "url": "/static/js/runtime-main.36cb7246.js"
+  }
+]);
